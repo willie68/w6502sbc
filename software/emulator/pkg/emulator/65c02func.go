4876b535cdf473183990bf3333990ac1f919a7fb
Wilfried Klaas <w.klaas@gmx.de>
2022-09-24 13:28:47 +0200

    WIP: smb, rmb mnemonics

--- software/emulator/pkg/emulator/65c02func.go
+++ software/emulator/pkg/emulator/65c02func.go
@@ -3,38 +3,114 @@ package emulator
 import "fmt"
 
 var cfunc = []func(*emu6502) string{
-	0x04: tsb_zp, 0x0c: tsb_abs, 0x0f: bbr0,
-	0x12: ora_zp_ind, 0x14: trb_zp, 0x1a: inc, 0x1c: trb_abs, 0x1f: bbr1,
-	0x2f: bbr2,
-	0x32: and_zp_ind, 0x34: bit_zp_x, 0x3a: dec, 0x3c: bit_abs_x, 0x3f: bbr3,
-	0x4f: bbr4,
-	0x52: eor_zp_ind, 0x5a: phy, 0x5f: bbr5,
-	0x64: stz_zp, 0x6f: bbr6,
-	0x72: adc_zp_ind, 0x74: stz_zp_x, 0x7a: ply, 0x7c: jmp_abs_x, 0x7f: bbr7,
-	0x80: bra, 0x89: bit_direct, 0x8f: bbs0,
-	0x92: sta_zp_ind, 0x9c: stz_abs, 0x9e: stz_abs_x, 0x9f: bbs1,
-	0xaf: bbs2,
-	0xb2: lda_zp_ind, 0xbf: bbs3,
-	0xcf: bbs4,
-	0xd2: cmp_zp_ind, 0xda: phx, 0xdf: bbs5,
-	0xef: bbs6,
-	0xf2: sbc_zp_ind, 0xfa: plx, 0xff: bbs7,
+	0x04: tsb_zp, 0x07: rmb0, 0x0c: tsb_abs, 0x0f: bbr0,
+	0x12: ora_zp_ind, 0x14: trb_zp, 0x17: rmb1, 0x1a: inc, 0x1c: trb_abs, 0x1f: bbr1,
+	0x27: rmb2, 0x2f: bbr2,
+	0x32: and_zp_ind, 0x34: bit_zp_x, 0x37: rmb3, 0x3a: dec, 0x3c: bit_abs_x, 0x3f: bbr3,
+	0x47: rmb4, 0x4f: bbr4,
+	0x52: eor_zp_ind, 0x57: rmb5, 0x5a: phy, 0x5f: bbr5,
+	0x64: stz_zp, 0x67: rmb6, 0x6f: bbr6,
+	0x72: adc_zp_ind, 0x74: stz_zp_x, 0x77: rmb7, 0x7a: ply, 0x7c: jmp_abs_x, 0x7f: bbr7,
+	0x80: bra, 0x87: smb0, 0x89: bit_direct, 0x8f: bbs0,
+	0x92: sta_zp_ind, 0x97: smb1, 0x9c: stz_abs, 0x9e: stz_abs_x, 0x9f: bbs1,
+	0xa7: smb2, 0xaf: bbs2,
+	0xb2: lda_zp_ind, 0xb7: smb3, 0xbf: bbs3,
+	0xc7: smb4, 0xcf: bbs4,
+	0xd2: cmp_zp_ind, 0xd7: smb5, 0xda: phx, 0xdf: bbs5,
+	0xe7: smb6, 0xef: bbs6,
+	0xf2: sbc_zp_ind, 0xf7: smb7, 0xfa: plx, 0xff: bbs7,
+}
+
+func i_mb_(e *emu6502, bit uint8, c bool) (uint16, string) {
+	zp, str := e.getZPAddress()
+	v := e.getMemory(zp)
+	if c {
+		v = v & (0xff ^ (0x01 << bit))
+	} else {
+		v = v | (0x01 << bit)
+	}
+	e.setMemory(zp, v)
+	return zp, str
 }
 
 func rmb0(e *emu6502) string {
-	zp, str := e.getZPAddress()
-	v := e.getMemory(zp)
-	v = v & 0xfe
-	e.setMemory(zp, v)
+	zp, str := i_mb_(e, 0, true)
 	return str + fmt.Sprintf("      rmb0 $%.2x", zp)
 }
 
 func smb0(e *emu6502) string {
-	zp, str := e.getZPAddress()
-	v := e.getMemory(zp)
-	v = v | 0x01
-	e.setMemory(zp, v)
+	zp, str := i_mb_(e, 0, false)
 	return str + fmt.Sprintf("      smb0 $%.2x", zp)
+}
+
+func rmb1(e *emu6502) string {
+	zp, str := i_mb_(e, 1, true)
+	return str + fmt.Sprintf("      rmb1 $%.2x", zp)
+}
+
+func smb1(e *emu6502) string {
+	zp, str := i_mb_(e, 1, false)
+	return str + fmt.Sprintf("      smb1 $%.2x", zp)
+}
+
+func rmb2(e *emu6502) string {
+	zp, str := i_mb_(e, 2, true)
+	return str + fmt.Sprintf("      rmb2 $%.2x", zp)
+}
+
+func smb2(e *emu6502) string {
+	zp, str := i_mb_(e, 2, false)
+	return str + fmt.Sprintf("      smb2 $%.2x", zp)
+}
+
+func rmb3(e *emu6502) string {
+	zp, str := i_mb_(e, 3, true)
+	return str + fmt.Sprintf("      rmb3 $%.2x", zp)
+}
+
+func smb3(e *emu6502) string {
+	zp, str := i_mb_(e, 3, false)
+	return str + fmt.Sprintf("      smb3 $%.2x", zp)
+}
+
+func rmb4(e *emu6502) string {
+	zp, str := i_mb_(e, 4, true)
+	return str + fmt.Sprintf("      rmb4 $%.2x", zp)
+}
+
+func smb4(e *emu6502) string {
+	zp, str := i_mb_(e, 4, false)
+	return str + fmt.Sprintf("      smb4 $%.2x", zp)
+}
+
+func rmb5(e *emu6502) string {
+	zp, str := i_mb_(e, 5, true)
+	return str + fmt.Sprintf("      rmb5 $%.2x", zp)
+}
+
+func smb5(e *emu6502) string {
+	zp, str := i_mb_(e, 5, false)
+	return str + fmt.Sprintf("      smb5 $%.2x", zp)
+}
+
+func rmb6(e *emu6502) string {
+	zp, str := i_mb_(e, 6, true)
+	return str + fmt.Sprintf("      rmb6 $%.2x", zp)
+}
+
+func smb6(e *emu6502) string {
+	zp, str := i_mb_(e, 6, false)
+	return str + fmt.Sprintf("      smb6 $%.2x", zp)
+}
+
+func rmb7(e *emu6502) string {
+	zp, str := i_mb_(e, 7, true)
+	return str + fmt.Sprintf("      rmb7 $%.2x", zp)
+}
+
+func smb7(e *emu6502) string {
+	zp, str := i_mb_(e, 7, false)
+	return str + fmt.Sprintf("      smb7 $%.2x", zp)
 }
 
 func i_bbx(e *emu6502, bit uint8, c bool) (uint16, string) {
@@ -235,7 +311,7 @@ func dec(e *emu6502) string {
 
 func bit_direct(e *emu6502) string {
 	v := e.getMnemonic()
-	i_bit(e, v)
+	e.zf = (v & e.a) == 0
 	return fmt.Sprintf("%.2x        bit #$%.2x", v, v)
 }
 
@@ -317,11 +393,11 @@ func sbc_zp_ind(e *emu6502) string {
 	adr := e.readVector(zp)
 	v := e.getMemory(adr)
 	sbc(e, v)
-	return str + fmt.Sprintf("     sbc ($%.2x)", adr)
+	return str + fmt.Sprintf("        sbc ($%.2x)", adr)
 }
 
 func jmp_abs_x(e *emu6502) string {
 	adr, str := e.getAddress()
 	e.address = e.readVector(adr + uint16(e.x))
-	return str + fmt.Sprintf("     jmp ($%.4x,X)", adr)
+	return str + fmt.Sprintf("        jmp ($%.4x,X)", adr)
 }
--- software/emulator/pkg/emulator/65c02func_test.go
+++ software/emulator/pkg/emulator/65c02func_test.go
@@ -7,6 +7,125 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestSmb0(t *testing.T) {
+	ast := assert.New(t)
+	data := []uint8{
+		0x20, 0x02, 0x21, 0x02, 0x00, 0x21,
+	}
+	e := getEmu(data)
+	e.ram.setMem(uint16(0x20), 0x00)
+	e.ram.setMem(uint16(0x21), 0xff)
+	smbs := []func(*emu6502) string{
+		smb0, smb1, smb2, smb3, smb4, smb5, smb6, smb7,
+	}
+	v := uint8(0x00)
+	for i, f := range smbs {
+
+		e.address = 0xe000
+		str := f(e)
+		fmt.Printf("smb%d output: %s\n\r", i, str)
+
+		ast.Equal(uint16(0xe001), e.address)
+		v = v + (0x01 << i)
+		ast.Equal(v, e.ram.getMem(uint16(0x0020)))
+	}
+
+	rmbs := []func(*emu6502) string{
+		rmb0, rmb1, rmb2, rmb3, rmb4, rmb5, rmb6, rmb7,
+	}
+	v = uint8(0xff)
+	for i, f := range rmbs {
+		e.address = 0xe002
+		str := f(e)
+		fmt.Printf("rmb%d output: %s\n\r", i, str)
+
+		ast.Equal(uint16(0xe003), e.address)
+		v = v - (0x01 << i)
+		ast.Equal(v, e.ram.getMem(uint16(0x0021)))
+	}
+}
+
+func TestBb_(t *testing.T) {
+	ast := assert.New(t)
+	data := []uint8{
+		0x20, 0x02, 0x21, 0x02, 0x00, 0x21,
+	}
+	e := getEmu(data)
+	e.ram.setMem(uint16(0x20), 0xff)
+	e.ram.setMem(uint16(0x21), 0x00)
+	bbrs := []func(*emu6502) string{
+		bbr0, bbr1, bbr2, bbr3, bbr4, bbr5, bbr6, bbr7,
+	}
+	for i, f := range bbrs {
+
+		e.address = 0xe000
+		str := f(e)
+		fmt.Printf("bbr%d output: %s\n\r", i, str)
+
+		ast.Equal(uint16(0xe002), e.address)
+
+		e.address = 0xe002
+		str = f(e)
+		fmt.Printf("bbr%d output: %s\n\r", i, str)
+
+		ast.Equal(uint16(0xe006), e.address)
+	}
+
+	bbss := []func(*emu6502) string{
+		bbs0, bbs1, bbs2, bbs3, bbs4, bbs5, bbs6, bbs7,
+	}
+	for i, f := range bbss {
+		e.address = 0xe000
+		str := f(e)
+		fmt.Printf("bbs%d output: %s\n\r", i, str)
+
+		ast.Equal(uint16(0xe004), e.address)
+
+		e.address = 0xe002
+		str = f(e)
+		fmt.Printf("bbs%d output: %s\n\r", i, str)
+
+		ast.Equal(uint16(0xe004), e.address)
+	}
+}
+
+func TestIBbx(t *testing.T) {
+	ast := assert.New(t)
+	data := []uint8{
+		0x20, 0x02, 0x21, 0x02, 0x00, 0x21,
+	}
+	e := getEmu(data)
+	e.ram.setMem(uint16(0x20), 0xff)
+	e.ram.setMem(uint16(0x21), 0x00)
+
+	for i := 0; i < 8; i++ {
+
+		e.address = 0xe000
+		v, _ := i_bbx(e, 0x01<<i, false)
+
+		ast.Equal(uint16(0xe004), e.address)
+		ast.Equal(uint16(0x20), v)
+
+		e.address = 0xe002
+		v, _ = i_bbx(e, 0x01<<i, false)
+
+		ast.Equal(uint16(0xe004), e.address)
+		ast.Equal(uint16(0x21), v)
+
+		e.address = 0xe000
+		v, _ = i_bbx(e, 0x01<<i, true)
+
+		ast.Equal(uint16(0xe002), e.address)
+		ast.Equal(uint16(0x20), v)
+
+		e.address = 0xe002
+		v, _ = i_bbx(e, 0x01<<i, true)
+
+		ast.Equal(uint16(0xe006), e.address)
+		ast.Equal(uint16(0x21), v)
+	}
+}
+
 func TestTsb(t *testing.T) {
 	ast := assert.New(t)
 	data := []uint8{
@@ -220,6 +339,79 @@ func TestDec(t *testing.T) {
 	testFlags(ast, nil, False, True, e)
 }
 
+func TestBitDirect(t *testing.T) {
+	ast := assert.New(t)
+	data := []uint8{
+		0x73, 0x70, 0x23,
+	}
+	e := getEmu(data)
+	e.a = 0x02
+	e.cf = false
+	e.zf = false
+	e.nf = false
+	str := bit_direct(e)
+	fmt.Printf("bit output: %s\n\r", str)
+
+	testFlags(ast, False, False, False, e)
+}
+
+func TestBitZpX(t *testing.T) {
+	ast := assert.New(t)
+	data := []uint8{
+		0x40, 0xe0, 0x00,
+	}
+	e := getEmu(data)
+	e.ram.data[0x43] = 0x03
+	e.x = 3
+
+	e.a = 0x04
+	e.cf = false
+	e.zf = false
+	e.nf = false
+
+	str := bit_zp_x(e)
+	fmt.Printf("bit output: %s\n\r", str)
+
+	testFlags(ast, False, True, False, e)
+}
+
+func TestBitAbsX(t *testing.T) {
+	ast := assert.New(t)
+	data := []uint8{
+		0x00, 0xe0, 0x03, 0x04,
+	}
+	e := getEmu(data)
+	e.ram.setMem(uint16(0x7070), uint8(0x00))
+	e.ram.setMem(uint16(0x7071), uint8(0xe0))
+	e.x = 3
+
+	e.a = 0x04
+	e.cf = false
+	e.zf = false
+	e.nf = false
+
+	str := bit_abs_x(e)
+	fmt.Printf("bit output: %s\n\r", str)
+
+	testFlags(ast, False, False, False, e)
+}
+
+func TestStaZpInd(t *testing.T) {
+	ast := assert.New(t)
+	data := []uint8{
+		0x70, 0x70, 0x23,
+	}
+	e := getEmu(data)
+	e.ram.setMem(uint16(0x0070), uint8(0x02))
+	e.ram.setMem(uint16(0x0071), uint8(0x70))
+	e.a = 0x73
+	str := sta_zp_ind(e)
+	fmt.Printf("sta output: %s\n\r", str)
+
+	ast.Equal(uint8(0x73), e.ram.getMem(uint16(0x7002)))
+	testFlags(ast, False, False, False, e)
+}
+
 func TestSbcZpInd(t *testing.T) {
 	ast := assert.New(t)
 	data := []uint8{
@@ -234,6 +426,106 @@ func TestSbcZpInd(t *testing.T) {
 	fmt.Printf("sbc output: %s\n\r", str)
 
 	ast.Equal(uint8(0x50), e.a)
+}
+
+func TestOraZpInd(t *testing.T) {
+	ast := assert.New(t)
+	data := []uint8{
+		0x70, 0x70, 0x23,
+	}
+	e := getEmu(data)
+	e.ram.setMem(uint16(0x0070), uint8(0x02))
+	e.ram.setMem(uint16(0x0071), uint8(0xe0))
+	e.a = 0x51
+	e.cf = false
+	str := ora_zp_ind(e)
+	fmt.Printf("ora output: %s\n\r", str)
+
+	ast.Equal(uint8(0x73), e.a)
+	testFlags(ast, False, False, False, e)
+}
+
+func TestLdaZpInd(t *testing.T) {
+	ast := assert.New(t)
+	data := []uint8{
+		0x70, 0x70, 0x23,
+	}
+	e := getEmu(data)
+	e.ram.setMem(uint16(0x0070), uint8(0x02))
+	e.ram.setMem(uint16(0x0071), uint8(0xe0))
+	str := lda_zp_ind(e)
+	fmt.Printf("lda output: %s\n\r", str)
+
+	ast.Equal(uint8(0x23), e.a)
+	testFlags(ast, False, False, False, e)
+}
+
+func TestEorZpInd(t *testing.T) {
+	ast := assert.New(t)
+	data := []uint8{
+		0x70, 0x70, 0x23,
+	}
+	e := getEmu(data)
+	e.ram.setMem(uint16(0x0070), uint8(0x02))
+	e.ram.setMem(uint16(0x0071), uint8(0xe0))
+	e.a = 0x71
+	e.cf = false
+	str := eor_zp_ind(e)
+	fmt.Printf("eor output: %s\n\r", str)
+
+	ast.Equal(uint8(0x52), e.a)
+	testFlags(ast, False, False, False, e)
+}
+
+func TestCmpZpInd(t *testing.T) {
+	ast := assert.New(t)
+	data := []uint8{
+		0x70, 0x70, 0x20,
+	}
+	e := getEmu(data)
+	e.ram.setMem(uint16(0x0070), uint8(0x02))
+	e.ram.setMem(uint16(0x0071), uint8(0xe0))
+	e.a = 0x70
+	e.cf = false
+	str := cmp_zp_ind(e)
+	fmt.Printf("cmp output: %s\n\r", str)
+
+	ast.Equal(uint8(0x70), e.a)
+	testFlags(ast, True, False, False, e)
+}
+
+func TestAndZpInd(t *testing.T) {
+	ast := assert.New(t)
+	data := []uint8{
+		0x70, 0x70, 0x20,
+	}
+	e := getEmu(data)
+	e.ram.setMem(uint16(0x0070), uint8(0x02))
+	e.ram.setMem(uint16(0x0071), uint8(0xe0))
+	e.a = 0x70
+	e.cf = false
+	str := and_zp_ind(e)
+	fmt.Printf("and output: %s\n\r", str)
+
+	ast.Equal(uint8(0x20), e.a)
+	testFlags(ast, False, False, False, e)
+}
+
+func TestAdcZpInd(t *testing.T) {
+	ast := assert.New(t)
+	data := []uint8{
+		0x70, 0x70, 0x20,
+	}
+	e := getEmu(data)
+	e.ram.setMem(uint16(0x0070), uint8(0x02))
+	e.ram.setMem(uint16(0x0071), uint8(0xe0))
+	e.a = 0x70
+	e.cf = false
+	str := adc_zp_ind(e)
+	fmt.Printf("adc output: %s\n\r", str)
+
+	ast.Equal(uint8(0x90), e.a)
+	testFlags(ast, False, False, True, e)
 }
 
 func TestJmpAbsX(t *testing.T) {
